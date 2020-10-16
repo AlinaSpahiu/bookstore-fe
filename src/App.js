28505819dfa8d19a8ed90d2ad3522699b437de78
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-10-16 23:47:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
 
 
 function App() {
   return (
     <>
-    <h1> Hello</h1>
+      <Header />
+        <main>
+           <h1> Welcome to BookStore</h1>
+        </main>
+      <Footer />
     </>
   );
 }
