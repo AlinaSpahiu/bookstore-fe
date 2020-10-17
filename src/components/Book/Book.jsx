86385043e0d1ae902862a309cf1274d72dde6a25
AlinaSpahiu <alina.s.abbaz@gmail.com>
2020-10-17 23:58:50 +0200

    Update Book.jsx

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 import { Card, Container, Row, Button } from "react-bootstrap";
 import "./Book.css";
 import Raiting from '../Raiting/Raiting'
@@ -6,19 +7,19 @@ import Raiting from '../Raiting/Raiting'
 const Book = ({ book }) => {
   return (
     <Row className="justify-content-center">
-      <Card style={{ width: "18rem" }}>
-        <a href={`/book/${book._id}`}>
-          {" "}
-          <Card.Img variant="top" src={book.image} />{" "}
-        </a>
+      <Card style={{ width: "15rem"}}>
+        <Link to={`/book/${book._id}`}>
+          <Card.Img variant="top" src={book.image} fluid/>
+        </Link>
 
         <Card.Body>
-          <a href={`/book/${book._id}`}>
+          <Link to={`/book/${book._id}`}>
             <Card.Title>{book.title}</Card.Title>
-          </a>
+          </Link>
 
           <Card.Text as="div">
             <Raiting value={book.rating} text={`${book.numReviews} reviews`}/>
+            <h5> Price: {book.price} € </h5>
           </Card.Text>
 
           <Button variant="primary w-50">Buy</Button>
