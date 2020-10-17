c2cd5466a4a1bb3db55ef707e63214069b16db80
AlinaSpahiu <alina.s.abbaz@gmail.com>
2020-10-17 23:58:32 +0200

    Update BooksPage.jsx

--- src/pages/BooksPage.jsx
+++ src/pages/BooksPage.jsx
@@ -6,16 +6,18 @@ import books from '../books';
 
 const BooksPage = () => {
     return (
+        <Container>
         <Row className="justify-content-center">
             <h1> Our Books!</h1>
             <Row>
                 {books.map(book => (
-                    <Col key={book._id} sm={12} md={6} lg={4} xl={2} className="">
+                    <Col key={book._id} sm={12} md={6} lg={4} xl={3} className="">
                        <Book book={book} />
                     </Col>
                 ))}
             </Row>
         </Row>
+        </Container>
     )
 }
 
