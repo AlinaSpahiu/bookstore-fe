88d2d73d0a4e8bbeaaaeabb5f5af1a2fe8434c53
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-10-17 16:49:22 +0200

    Create books.js

---
+++ src/books.js
@@ -0,0 +1,81 @@
+const books = [
+    {
+        _id: '1',
+        title:'Green Hills of Africa',
+        author:'Ernest Heminguej',
+        description:'...',
+        category:'roman',
+        price: 10.60,
+        countInStock: 4,
+        rating: 2.0,
+        numReviews: 6,
+        image: "/images/bookImg/greenhills.jpg",
+    },
+
+    {
+        _id: '2',
+        title:'Old man and the sea',
+        author:'Ernest Heminguej',
+        description:'...',
+        category:'roman',
+        price: 9.60,
+        countInStock: 2,
+        rating: 0.5,
+        numReviews: 9,
+        image: "/images/bookImg/oldman.jpg"
+    },
+
+    {
+        _id: '3',
+        title:'Green Hills of Africa',
+        author:'Ernest Heminguej',
+        description:'...',
+        category:'roman',
+        price: 10.60,
+        countInStock: 4,
+        rating: 4.5,
+        numReviews: 2,
+        image: "/images/bookImg/greenhills.jpg"
+    },
+
+    {
+        _id: '4',
+        title:'Green Hills of Africa',
+        author:'Ernest Heminguej',
+        description:'...',
+        category:'roman',
+        price: 10.60,
+        countInStock: 4,
+        rating: 1.5,
+        numReviews: 13,
+        image: "/images/bookImg/greenhills.jpg"
+    },
+
+    {
+        _id: '5',
+        title:'Old man and the sea',
+        author:'Ernest Heminguej',
+        description:'...',
+        category:'roman',
+        price: 9.60,
+        countInStock: 2,
+        rating: 4.0,
+        numReviews: 15,
+        image: "/images/bookImg/oldman.jpg"
+    },
+
+    {
+        _id: '6',
+        title:'Old man and the sea',
+        author:'Ernest Heminguej',
+        description:'...',
+        category:'roman',
+        price: 9.60,
+        countInStock: 2,
+        rating: 2.5,
+        numReviews: 29,
+        image: "/images/bookImg/oldman.jpg"
+    },
+]
+
+export default books
