99eb41ffb847f78ebdbd694632154711e6d74616
AlinaSpahiu <alina.s.abbaz@gmail.com>
2020-10-17 23:58:46 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,45 @@
 import React from "react";
-import { Form, FormControl, Button, Navbar, Nav } from "react-bootstrap";
+import { Form, FormControl, Button, Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap'
 import "./Header.css"
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="/">BookStore</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Container>
+          <LinkContainer to='/' >
+            <Navbar.Brand>BookStore</Navbar.Brand>
+          </LinkContainer>
 
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
 
-          <Nav className="links">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/books">Books</Nav.Link>
-            <Nav.Link href="/cart">Cart<i className="fas fa-shopping-cart ml-"></i></Nav.Link>
-            <Nav.Link href="/cart">Login<i className="fas fa-user ml-1"></i></Nav.Link>
-          </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
 
-        </Navbar.Collapse>
+            <Nav className="links">
+              <LinkContainer to='/'>
+                <Nav.Link> Home </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to='/books'>
+                <Nav.Link> Books </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to='/cart'>
+                <Nav.Link> Cart <i className="fas fa-shopping-cart ml-"></i> </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to='/login'>
+                <Nav.Link> Login <i className="fas fa-user ml-1"></i> </Nav.Link>
+              </LinkContainer>
+            </Nav>
+
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </header>
   );
