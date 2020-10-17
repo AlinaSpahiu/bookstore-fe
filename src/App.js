aae397a411c8069fee5f8be611ff9ea021d05e11
AlinaSpahiu <alina.s.abbaz@gmail.com>
2020-10-17 23:58:12 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-
 import BooksPage from "./pages/BooksPage";
+import BookDetailsPage from './pages/BookDetailsPage/BookDetailsPage'
 
 
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
         <>
-           <BooksPage />
+           <Route path='/books' component={BooksPage} exact />
+           <Route path='/book/:id' component={BookDetailsPage} exact />
         </>
       <Footer />
-    </>
+    </Router>
   );
 }
 
