efe0a45e617b3585855e6f28e6c094b1ee690388
AlinaSpahiu <alina.s.abbaz@gmail.com>
2020-10-18 22:23:10 +0200

    Update BookDetailsPage.jsx

--- src/pages/BookDetailsPage/BookDetailsPage.jsx
+++ src/pages/BookDetailsPage/BookDetailsPage.jsx
@@ -1,23 +1,44 @@
-import React from 'react'
+import React,  {useState, useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Card, Button, Container} from 'react-bootstrap'
 import Raiting from '../../components/Raiting/Raiting'
-import books from '../../books'
+import {listBookDetails} from '../../actions/bookActions'
+import Loader from '../../components/MessageLoader/Loader'
+import Message from '../../components/MessageLoader/Message'
+
 
 const BookDetailsPage = ({match}) => {
-    const book = books.find(p => p._id === match.params.id)
+
+      const dispatch = useDispatch()
+      const bookDetails = useSelector(state => state.bookDetails)
+      const {loading, error, book}  = bookDetails
+ 
+      useEffect(() =>{
+
+        dispatch(listBookDetails(match.params.id) )
+      
+      }, [dispatch, match])
+    
     return (
         <>
+            { loading ? < Loader /> 
+             : error ? <Message> { error}</Message>
+             : 
         <Container>
            <Link className='btn btn-dark my-3' to='/books'>Go Back</Link>
 
+       
+                 
+           
+
            <Row className='justify-content-center my-3'>
-               {/* IMAGE */}
+               
                <Col md={6}>
                    <Image src={book.image} alt={book.title} fluid style={{height:'50vh', width:'80%'}}/>
                </Col>
                 
-                {/* DESCRIPTION */}
+            
                <Col md={6}>
                    <ListGroup variant='flush'>
                        <ListGroup.Item>
@@ -39,7 +60,7 @@ const BookDetailsPage = ({match}) => {
                </Col>
                </Row>
 
-               {/* ADD TO CART */}
+            
                <Row className='justify-content-center'>
                  <Card className='w-75 '>
                      <ListGroup.Item>
@@ -57,12 +78,13 @@ const BookDetailsPage = ({match}) => {
                      </ListGroup.Item>
 
                      <ListGroup.Item>
-                         <Button className='btn-block' type='button'> Add To Cart</Button>
+                         <Button className='btn-block' type='button' disabled= {book.countInStock === 0}> Add To Cart</Button>
                      </ListGroup.Item>
                  </Card>
                </Row>
-           
+              
         </Container>
+})
         </>
     )
 }
