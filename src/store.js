c9cbf3b3ac10e0e62b694237558bbaa3ce3ebe98
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-10-20 20:10:53 +0200

    Update store.js

--- src/store.js
+++ src/store.js
@@ -3,18 +3,27 @@ import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {bookListReducer, bookDetailsReducer} from './reducers/bookReducers'
 import {cartReducer} from './reducers/cartReducers'
+import {userLoginReducer} from "./reducers/userReducers"
 
 
 const reducer = combineReducers({
     bookList: bookListReducer,
     bookDetails: bookDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userLogin: userLoginReducer
 
 })
 
+// Local Storage of CART
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+// Local Storage of USER
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
+// intialState:
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage}
+    cart: { cartItems: cartItemsFromStorage},
+    userLogin: {userInfo: userInfoFromStorage}
 }
 
 const middleware = [thunk]
