022cc839524a273d1c285d378b95d147c9eb6404
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-10-26 22:51:47 +0100

    Update userActions.js

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -9,7 +9,11 @@ import { USER_LOGIN_FAIL,
          
          USER_DETAILS_REQUEST,
          USER_DETAILS_SUCCESS,
-         USER_DETAILS_FAIL} 
+         USER_DETAILS_FAIL,
+         
+         USER_UPDATE_PROFILE_SUCCESS,
+         USER_UPDATE_PROFILE_FAIL,
+         USER_UPDATE_PROFILE_REQUEST} 
 from "../constants/userConstants"
 import axios from 'axios'
 
@@ -114,6 +118,7 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
          Authorization: `Bearer ${userInfo.token}`
       },
     }
+  
     const { data } = await axios.get(
       `/api/users/${id}`,
       config
@@ -126,6 +131,42 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: USER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+
+// ~~~
+// User Update Profile Action
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_UPDATE_PROFILE_REQUEST,
+    })
+    
+    const { userLogin: {userInfo} } = getState()
+    
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+         Authorization: `Bearer ${userInfo.token}`
+      },
+    }
+  
+    const { data } = await axios.put(
+      `/api/users/profile`, user, config )
+
+    dispatch({
+      type: USER_UPDATE_PROFILE_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: USER_UPDATE_PROFILE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
