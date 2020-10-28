c1bd37a0c6d9d0a1e113d28db2ca095f998d65e2
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-10-28 23:14:05 +0100

    Update orderReducers.js

--- src/reducers/orderReducers.js
+++ src/reducers/orderReducers.js
@@ -5,7 +5,12 @@ import {
         
         ORDER_DETAILS_REQUEST,
         ORDER_DETAILS_SUCCESS,
-        ORDER_DETAILS_FAIL
+        ORDER_DETAILS_FAIL,
+
+        ORDER_PAY_REQUEST,
+        ORDER_PAY_SUCCESS,
+        ORDER_PAY_FAIL,
+        ORDER_PAY_RESET
 } from '../constants/orderConstants'
 
 
@@ -22,7 +27,6 @@ export const orderCreateReducer = (state={}, action) => {
         default:
              return state
     }
-
 }
 
 
@@ -39,5 +43,22 @@ export const orderDetailsReducer = (state={ loading: true, orderItems: [], shipp
          default:
               return state
      }
- 
+ }
+
+
+ // ~~~
+// Order Pay Reducer
+export const orderPayReducer = (state={}, action) => {
+     switch(action.type){
+         case ORDER_PAY_REQUEST:
+              return { loading: true }
+         case ORDER_PAY_SUCCESS:
+              return { loading: false, success: true}
+         case ORDER_PAY_FAIL:
+              return { loading: false, error: action.payload}
+          case ORDER_PAY_RESET:
+               return {}
+         default:
+              return state
+     }
  }
