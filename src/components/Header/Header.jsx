c5fac1c3b00299e4450aa3b22111894f89fd7233
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-10-29 23:57:46 +0100

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
   }
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar bg='dark' variant='dark' expand="lg">
         <Container>
           <LinkContainer to='/' >
             <Navbar.Brand>BookStore</Navbar.Brand>
